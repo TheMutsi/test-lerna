1ae4d37088a1b9142ec6b88eb112fbeb28a3b8e5
TheMutsi <33431136+TheMutsi@users.noreply.github.com>
2025-11-25 23:33:23 +0900

    New button

--- cxComponents.js
+++ cxComponents.js
@@ -1,1 +1,5 @@
 var ComponentStrategy = {};
+
+ComponentStrategy.rrrrrr = function rrrrrr() {
+    return `<button style="background-color: red; color: white; border: none; padding: 10px 20px; cursor: pointer;">rrrrrr</button>`;
+};
