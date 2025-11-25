ea1097421a407de14433063a9522998ba8463159
TheMutsi <33431136+TheMutsi@users.noreply.github.com>
2025-11-25 23:34:49 +0900

    New button

--- cxComponents.js
+++ cxComponents.js
@@ -3,3 +3,4 @@ var ComponentStrategy = {};
 ComponentStrategy.rrrrrr = function rrrrrr() {
     return `<button style="background-color: red; color: white; border: none; padding: 10px 20px; cursor: pointer;">rrrrrr</button>`;
 };
+ComponentStrategy.report = function report() { return `<button id="report_01" style="background-color: red; border-radius: 8px; border: none; padding: 10px 20px; cursor: pointer;" onclick="console.log('anda bien')">report</button>`; };
